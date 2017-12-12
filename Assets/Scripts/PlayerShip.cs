20465fa90bed8f50d14562c82eb45189ebbf73a6
KeenanMau <keenan.mau@gmail.com>
2017-12-12 13:58:25 -0800

    Start of Save
    Creation of project folders, inclusion game assets, code for moving blue
    player ship with speed.

---
+++ Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShip : MonoBehaviour {
+
+    public float speed;
+    Vector3 ship_position;
+    public float minX, maxX;
+    // Use this for initialization
+    void Start () {
+        speed = 1;
+        ship_position = new Vector3(0.5f, this.transform.position.y, 0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        moveShip();
+        
+    }
+
+    void moveShip()
+    {
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+			this.transform.position +=  Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+			this.transform.position +=  Vector3.left * speed * Time.deltaTime;
+        }
+
+        
+    }
+}
